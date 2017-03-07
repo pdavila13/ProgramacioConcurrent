1f72cf1ee4fb0e1d0dcf0c63194f21b052bedb90
Paolo Dávila <pdavila@iesebre.com>
2017-03-07 20:13:27 +0100

    Return bbc

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
+// return b
 function b() {
   console.log('b');
 }
 
 b()
+
+// return ¿? --> b
+b(function() {
+  console.log('a');
+})
+
+// return c
+function c(x) {
+  console.log('c');
+}
+
+c()
